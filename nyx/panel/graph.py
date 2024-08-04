1df8f43bb849f242fbd637d8365e06645fba18cd
Omar Murad <me@omar-murad.com>
2024-08-04 02:02:45 -0400

    ...

--- nyx/panel/graph.py
+++ nyx/panel/graph.py
@@ -138,114 +138,6 @@ class GraphData(object):
       self._is_primary = clone._is_primary
       self._in_process_value = dict(clone._in_process_value)
       self._max_value = dict(clone._max_value)
-      self._first_update = True  # Flag to track the first update call
-    else:
-      self.latest_value = 0
-      self.total = 0
-      self.tick = 0
-      self.values = dict([(i, CONFIG['max_graph_width'] * [0]) for i in Interval])
-
-      self._category = category
-      self._is_primary = is_primary
-      self._in_process_value = dict([(i, 0) for i in Interval])
-      self._max_value = dict([(i, 0) for i in Interval])  # interval => maximum value it's had
-      self._first_update = True  # Flag to track the first update call
-
-  def average(self):
-    return self.total / max(1, self.tick)
-
-  def update(self, new_value):
-    if not self._first_update:
-            self.latest_value = new_value
-    else:
-      self._first_update = False
-    
-    self.total += new_value
-    self.tick += 1
-
-    for interval in Interval:
-      interval_seconds = INTERVAL_SECONDS[interval]
-      self._in_process_value[interval] += new_value
-
-      if self.tick % interval_seconds == 0:
-        new_entry = self._in_process_value[interval] / interval_seconds
-        self.values[interval] = [new_entry] + self.values[interval][:-1]
-        self._max_value[interval] = max(self._max_value[interval], new_entry)
-        self._in_process_value[interval] = 0
-
-  def header(self, width):
-    """
-    Provides the description above a subgraph.
-
-    :param int width: maximum length of the header
-
-    :returns: **str** with our graph header
-    """
-
-    return self._category._header(width, self._is_primary)
-
-  def bounds(self, bounds, interval, columns):
-    """
-    Range of values for the graph.
-
-    :param Bounds bounds: boundary type for the range we want
-    :param Interval interval: timing interval of the values
-    :param int columns: number of values to take into account
-
-    :returns: **tuple** of the form (min, max)
-    """
-
-    min_bound, max_bound = 0, 0
-    values = self.values[interval][:columns]
-
-    if bounds == Bounds.GLOBAL_MAX:
-      max_bound = self._max_value[interval]
-    elif columns > 0:
-      max_bound = max(values)  # local maxima
-
-    if bounds == Bounds.TIGHT and columns > 0:
-      min_bound = min(values)
-
-      # if the max = min pick zero so we still display something
-
-      if min_bound == max_bound:
-        min_bound = 0
-
-    return min_bound, max_bound
-
-  def y_axis_label(self, value):
-    """
-    Provides the label we should display on our y-axis.
-
-    :param int value: value being shown on the y-axis
-
-    :returns: **str** with our y-axis label
-    """
-
-    return self._category._y_axis_label(value, self._is_primary)
-
-
-class GraphData(object):
-  """
-  Graphable statistical information.
-
-  :var int latest_value: last value we recorded
-  :var int total: sum of all values we've recorded
-  :var int tick: number of events we've processed
-  :var dict values: mapping of intervals to an array of samplings from newest to oldest
-  """
-
-  def __init__(self, clone = None, category = None, is_primary = True):
-    if clone:
-      self.latest_value = clone.latest_value
-      self.total = clone.total
-      self.tick = clone.tick
-      self.values = copy.deepcopy(clone.values)
-
-      self._category = category
-      self._is_primary = clone._is_primary
-      self._in_process_value = dict(clone._in_process_value)
-      self._max_value = dict(clone._max_value)
     else:
       self.latest_value = 0
       self.total = 0
@@ -331,6 +223,76 @@ class GraphData(object):
 
     return self._category._y_axis_label(value, self._is_primary)
 
+
+class GraphCategory(object):
+  """
+  Category for the graph. This maintains two subgraphs, updating them each
+  second with updated stats.
+
+  :var GraphData primary: first subgraph
+  :var GraphData secondary: second subgraph
+  :var float start_time: unix timestamp for when we started
+  """
+
+  def __init__(self, clone = None):
+    if clone:
+      self.primary = GraphData(clone.primary, category = self)
+      self.secondary = GraphData(clone.secondary, category = self)
+      self.start_time = clone.start_time
+      self._title_stats = list(clone._title_stats)
+      self._primary_header_stats = list(clone._primary_header_stats)
+      self._secondary_header_stats = list(clone._secondary_header_stats)
+    else:
+      self.primary = GraphData(category = self, is_primary = True)
+      self.secondary = GraphData(category = self, is_primary = False)
+      self.start_time = time.time()
+      self._title_stats = []
+      self._primary_header_stats = []
+      self._secondary_header_stats = []
+
+  def stat_type(self):
+    """
+    Provides the GraphStat this graph is for.
+
+    :returns: **GraphStat** of this graph
+    """
+
+    raise NotImplementedError('Should be implemented by subclasses')
+
+  def title(self, width):
+    """
+    Provides a graph title that fits in the given width.
+
+    :param int width: maximum length of the title
+
+    :returns: **str** with our title
+    """
+
+    title = CONFIG['attr.graph.title'].get(self.stat_type(), '')
+    title_stats = join(self._title_stats, ', ', width - len(title) - 4)
+    return '%s (%s):' % (title, title_stats) if title_stats else title + ':'
+
+  def bandwidth_event(self, event):
+    """
+    Called when it's time to process another event. All graphs use tor BW
+    events to keep in sync with each other (this happens once per second).
+    """
+
+    pass
+
+  def _header(self, width, is_primary):
+    if is_primary:
+      header = CONFIG['attr.graph.header.primary'].get(self.stat_type(), '')
+      header_stats = self._primary_header_stats
+    else:
+      header = CONFIG['attr.graph.header.secondary'].get(self.stat_type(), '')
+      header_stats = self._secondary_header_stats
+
+    header_stats = join(header_stats, '', width - len(header) - 4).rstrip()
+    return '%s (%s):' % (header, header_stats) if header_stats else '%s:' % header
+
+  def _y_axis_label(self, value, is_primary):
+    return str(value)
 
 
 class BandwidthStats(GraphCategory):
